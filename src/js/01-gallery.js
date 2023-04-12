bad1ba7f1261072feafcf3e6de34a44988143e69
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-04-12 21:10:22 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,5 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import "simplelightbox/dist/simple-lightbox.min.css"
 console.log(galleryItems);
-ррр
+const listItem = document.querySelector(".gallery");
+const cardListItem = ({ original, preview, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${ preview}"
+        data-source="${original}"
+          alt="${description}"
+        />
+        </a>
+</div>`;};
+const cardListItemNew = galleryItems.map(cardListItem).join("");
+const cardsMarkup = cardListItem (galleryItems);
+listItem.addEventListener(`click`, onPaletteContainerClick);
+listItem.insertAdjacentHTML("beforeend", cardListItemNew);
+console.log(cardListItemNew); 
+function onPaletteContainerClick(evt) {
+  evt.preventDefault()
+  const instance = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
+  
+}
+
+console.log(galleryItems);
