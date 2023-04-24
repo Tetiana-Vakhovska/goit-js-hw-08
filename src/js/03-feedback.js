afe7bf66f7df95d3fda457c7951ac4556d6e60d0
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-04-24 23:00:16 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,41 @@
 import throttle from 'lodash.throttle';
-
 const STORAGE_KEY = 'feedback-form-state';
-
-const form = document.querySelector('.feedback-form');
+const refs ={
+  form : document.querySelector('.js-feedback-form'),
+  textarea:document.querySelector('.js-feedback-form area')
+}
 
 form.addEventListener('input', throttle(onInputData, 500));
 form.addEventListener('submit', onFormSubmit);
-
 let dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 const { email, message } = form.elements;
 reloadPage();
-
 function onInputData(event) {
   dataForm = { email: email.value, message: message.value };
   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
 }
-
 function reloadPage() {
   if (dataForm) {
     email.value = dataForm.email || '';
     message.value = dataForm.message || '';
   }
 }
-
 function onFormSubmit(event) {
   event.preventDefault();
   console.log({ email: email.value, message: message.value });
-
   if (email.value === '' || message.value === '') {
     return alert(`Будь ласка, заповніть всі обов'язкові поля.`);
    
   }
-
   localStorage.removeItem(STORAGE_KEY);
   event.currentTarget.reset();
   dataForm = {};
+}
+function populateTextareal(){
+const savedMessage = localStorage.getItem('feedback-msg');
+if (savedMessage){
+  console.log(savedMessage);
+  form=savedMessage;
+}
+
 }
